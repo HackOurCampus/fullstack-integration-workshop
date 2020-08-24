a5154670a9ffb893b75e6cdcd8dd4c7ea138b6ed
Megan Yin <my474@cornell.edu>
2020-08-24 15:51:01 -0400

    add products

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+PRODUCTS = [
+  { 'category': 'Sporting Goods', 'price': '$49.99', 'stocked': True, 'name': 'Football' },
+  { 'category': 'Sporting Goods', 'price': '$9.99', 'stocked': True, 'name': 'Baseball' },
+  { 'category': 'Sporting Goods', 'price': '$29.99', 'stocked': False, 'name': 'Basketball' },
+  { 'category': 'Electronics', 'price': '$99.99', 'stocked': True, 'name': 'iPod Touch' },
+  { 'category': 'Electronics', 'price': '$399.99', 'stocked': False, 'name': 'iPhone 5' },
+  { 'category': 'Electronics', 'price': '$199.99', 'stocked': True, 'name': 'Nexus 7' }
+]
+
+@app.route('/products')
+def get_all_products():
+    return {'products': PRODUCTS}
