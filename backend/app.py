ad472f891b3f772fb59642ef08fd0d1b8c8d002e
ashneeldas2 <ashneel.das@gmail.com>
2020-08-28 18:03:12 -0400

    Add CORS fix

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 import time
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 PRODUCTS = [
   { 'category': 'Sporting Goods', 'price': '$49.99', 'stocked': True, 'name': 'Football' },
